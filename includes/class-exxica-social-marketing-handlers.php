6cf2da33b866250d9e022c82f90cc953c5e9e39c
exxica <exxica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 08:09:22 +0000

    v1.1.8.3
    git-svn-id: https://plugins.svn.wordpress.org/exxica-social-marketing/tags/1.1.8.3@1147840 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-exxica-social-marketing-handlers.php
+++ includes/class-exxica-social-marketing-handlers.php
@@ -1010,6 +1010,7 @@ class Exxica_Postdata_Handler
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2 );
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE );
             curl_setopt($ch, CURLOPT_POST, TRUE );
+            curl_setopt($ch, CURLOPT_PORT, 80);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query( $atts ) );
             //print_r(http_build_query( $atts ) );
             $out = curl_exec($ch);
